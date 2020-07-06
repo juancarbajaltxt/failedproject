6487439eceded77c34054b5196462955c30fd417
TXT-52 <txt-52@MacBook-Pro-5.lan1>
2020-07-06 09:31:06 -0700

    broken routing

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -13,10 +13,10 @@ const routes: Routes = [
       },
       {
         path: 'tab2',
-        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+        loadChildren: () => import('../tab2/tab2').then(m => m.Tab2PageModule)
       },
       {
-        path: 'tab3',
+        path: 'tab-to-tab3',
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
       },
       {
